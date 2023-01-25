9fa444bbe60ed8add0fdb5cd3d4cd1b127685068
yeseul0722 <pdy051257@naver.com>
2023-01-25 17:10:35 +0900

    practice

--- 0125/데일리+실습_python_06_1_P.py
+++ 0125/데일리+실습_python_06_1_P.py
@@ -1,5 +1,41 @@
+# 1. 주민등록번호 앞의 6자리는 생년월일이고, 뒤 7자리는 성별, 지역, 오류검출코드이다.     
+# 주민등록번호를 입력받아 주민등록번호 뒷자리를 비식별화하는 함수 de_identify 를 만들어라.
+# 해당 변수는 인자를 하나만 받는다.
+# 함수 정의시 매개변수는 한 개면 된다.
+def de_identify(string):
+    # 문자열을 받아온 다음, 그 문자열이 가진 내용중
+    # '-'에 해당하는 부분을 없앨 것이다.
+    # 반복문을 통해서 넘겨받은 문자열을 하나하나 비교한 뒤, 특정 문자열을 제외한
+    # 문자열을 만들어서 반환해야 한다.
+    # result = ''
+    # for char in stirng:
+    #     if char == '-':
+    #         continue
+    #     else:
+    #         result += char
+
+    #문자열이 가진 특성상 원본을 바꿀수는 없으니 변경된 값을 반환
+    result = string.replace('-','')
+
+    print(result[:6] + '*' * 7) #'970103 + *******'
+
+    result = list(result)
+    result[6:] = ['*'] * 7
+    return ''.join(result)
+    # print(result)
+
+    # real_result = ''
+    # for char in result:
+    #     idx += 1
+    #     if idx >= 6:
+    #         real_result += '*'
+    #     else:
+    #         real_result += char
+    #     idx += 1
+    # print(result)
+
 # A. 입력예시
-# print(de_identify('970103-1234567'))
+print(de_identify('970103-1234567'))
 # print(de_identify('8611232345678'))
 
 # B. 출력예시
--- 0125/데일리+실습_python_06_2_P.py
+++ 0125/데일리+실습_python_06_2_P.py
@@ -1,1 +1,14 @@
 grain_lst = [('고구마',3000), ('감자',2000), ('옥수수',4500),('토란',1300)]
+# print(grain_lst.sort()) #None 반환
+# print(grain_lst) # 정렬된 리스트 확인 /[('감자', 2000), ('고구마', 3000), ('옥수수', 4500), ('토란', 1300)]
+# grain_dict = dict(grain_lst)
+# key = list(grain_dict.keys())
+# value = list(grain_dict.values())
+# max_value = 
+# print(value.index(max(value))) # 2 # print(value.index(4500))
+
+# max_idx = value.index(max(value))
+# print(key[max_idx])
+# print(key, value) #dict_keys(['감자', '고구마', '옥수수', '토란']) dict_values([2000, 3000, 4500, 1300])
+grain_lst.sort(key=lambda x : x[1], reverse=True) #reverse는 역순으로
+print(grain_lst[0][0])
